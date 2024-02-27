42cfed4c8ac1b15d45b317025c0c6291e11f32a1
marce-moya <marcelamoya@gmail>
2024-02-27 02:39:37 -0300

    reordenamiento de carpetas y archivos

--- Dashboard/src/index.js
+++ Dashboard/src/index.js
@@ -1,31 +1,42 @@
-// Llamada a la API
-const apiUrl = 'https://covid.ourworldindata.org/data/owid-covid-data.json';
+document.addEventListener('DOMContentLoaded', function () {
+    const apiUrl = 'https://restcountries.com/v3.1/all';
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log('Datos globales de COVID-19:', data);
-    
-    // Creación del gráfico
-    const ctx = document.getElementById('myChart');
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => displayCountries(data))
+        .catch((error) => console.error('Error en obtener lista:', error));
 
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  })
-  .catch(error => console.error('Error al obtener datos:', error));
+    function displayCountries(countries) {
+        const countriesListElement = document.getElementById('countries-list');
+        const searchInput = document.getElementById('search-input');
+
+        const ul = document.createElement('ul');
+
+        countries.forEach((country) => {
+            const li = document.createElement('li');
+            li.textContent = country.name.common;
+            ul.appendChild(li);
+        });
+
+        countriesListElement.appendChild(ul);
+
+        // Agregar un evento de escucha al campo de búsqueda
+        searchInput.addEventListener('input', function () {
+            const searchTerm = searchInput.value.toLowerCase();
+            const filteredCountries = countries.filter(country =>
+                country.name.common.toLowerCase().includes(searchTerm)
+            );
+
+            // Limpiar la lista actual antes de mostrar la lista filtrada
+            ul.innerHTML = '';
+
+            // Mostrar la lista filtrada
+            filteredCountries.forEach((country) => {
+                const li = document.createElement('li');
+                li.textContent = country.name.common;
+                ul.appendChild(li);
+            });
+        });
+    }
+});
+
